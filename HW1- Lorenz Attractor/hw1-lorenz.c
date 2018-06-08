ad7d47fbc7f6760681224333426333271e3d49b2
Justin Chin <justinchin.bcchs@gmail.com>
2018-06-07 23:11:42 -0600

    plotting lorenz values on graph

--- HW1- Lorenz Attractor/hw1-lorenz.c
+++ HW1- Lorenz Attractor/hw1-lorenz.c
@@ -16,6 +16,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+
 //  OpenGL with prototypes for glext
 #define GL_GLEXT_PROTOTYPES
 #ifdef __APPLE__
@@ -28,10 +29,23 @@
 int th=0;       // Azimuth of view angle
 int ph=0;       // Elevation of view angle
 int mode=1;     // Dimension (1-4)
-double z=0;     // Z variable
+double zz=0;     // Z variable
 double w=1;     // W variable
-double dim=2;   // Dimension of orthogonal box
+double dim=100;   // Dimension of orthogonal box
 char* text[] = {"","2D","3D constant Z","3D","4D"};  // Dimension display text
+
+/*  Lorenz Parameters  */
+double s  = 10;
+double b  = 2.6666;
+double r  = 28;
+
+int i;
+/*  Coordinates  */
+double x = 1;
+double y = 1;
+double z = 1;
+/*  Time step  */
+double dt = 0.001;
 
 /*
  *  Convenience routine to output raster text
@@ -64,14 +78,15 @@ void display()
    //  Set view angle
    glRotated(ph,1,0,0);
    glRotated(th,0,1,0);
+   
    //  Draw 10 pixel yellow points
    glColor3f(1,1,0);
-   glPointSize(10);
+   glPointSize(5);
    glBegin(GL_POINTS);
    switch (mode)
    {
    //  Two dimensions
-   case 1:
+   case 2:
       glVertex2d(0.1,0.1);
       glVertex2d(0.3,0.3);
       glVertex2d(0.5,0.5);
@@ -79,12 +94,36 @@ void display()
       glVertex2d(0.9,0.9);
       break;
    //  Three dimensions - constant Z
-   case 2:
-      glVertex3d(0.1,0.1,z);
-      glVertex3d(0.3,0.3,z);
-      glVertex3d(0.5,0.5,z);
-      glVertex3d(0.7,0.7,z);
-      glVertex3d(0.9,0.9,z);
+   case 1:
+      glVertex3d(0.0,0.0,0.0);
+      glVertex3d(1.0,1.0,1.0);
+
+      glVertex3d(-1.0,-1.0,-1.0);   
+
+	   //printf("%5d %8.3f %8.3f %8.3f\n",0,x,y,z);
+	   /*
+	    *  Integrate 50,000 steps (50 time units with dt = 0.001)
+	    *  Explicit Euler integration
+	    */
+	   glVertex3d(x,y,z);
+	   for (i=0;i<50000;i++)
+	   {
+	      double dx = s*(y-x);
+	      double dy = x*(r-z)-y;
+	      double dz = x*y - b*z;
+	      x += dt*dx;
+	      y += dt*dy;
+	      z += dt*dz;
+	      glVertex3d(x,y,z);
+	      //printf("%5d %8.3f %8.3f %8.3f\n",i+1,x,y,z);
+	   }	
+
+
+      //glVertex3d(0.1,0.1,z);
+      //glVertex3d(0.3,0.3,z);
+      //glVertex3d(0.5,0.5,z);
+      //glVertex3d(0.7,0.7,z);
+      //glVertex3d(0.9,0.9,z);
       break;
    //  Three dimensions - variable Z
    case 3:
@@ -108,29 +147,31 @@ void display()
    glColor3f(1,1,1);
    glBegin(GL_LINES);
    glVertex3d(0,0,0);
-   glVertex3d(1,0,0);
+   glVertex3d(90,0,0);
    glVertex3d(0,0,0);
-   glVertex3d(0,1,0);
+   glVertex3d(0,90,0);
    glVertex3d(0,0,0);
-   glVertex3d(0,0,1);
+   glVertex3d(0,0,90);
    glEnd();
    //  Label axes
-   glRasterPos3d(1,0,0);
+   glRasterPos3d(90,0,0);
    Print("X");
-   glRasterPos3d(0,1,0);
+   glRasterPos3d(0,90,0);
    Print("Y");
-   glRasterPos3d(0,0,1);
+   glRasterPos3d(0,0,90);
    Print("Z");
    //  Display parameters
    glWindowPos2i(5,5);
    Print("View Angle=%d,%d  %s",th,ph,text[mode]);
    if (mode==2)
-      Print("  z=%.1f",z);
+      Print("  z=%.1f",zz);
    else if (mode==4)
       Print("  w=%.1f",w);
    //  Flush and swap
    glFlush();
    glutSwapBuffers();
+
+
 }
 
 /*
@@ -148,14 +189,14 @@ void key(unsigned char ch,int x,int y)
    else if ('1'<=ch && ch<='4')
    {
       mode = ch-'0';
-      if (mode==2) z = 0;
+      if (mode==2) zz = 0;
       if (mode==4) w = 1;
    }
    //  Increase w by 0.1
    else if (ch == '+')
    {
       if (mode==2)
-         z += 0.1;
+         zz += 0.1;
       else
          w += 0.1;
    }
@@ -163,11 +204,15 @@ void key(unsigned char ch,int x,int y)
    else if (ch == '-')
    {
       if (mode==2)
-         z -= 0.1;
+         zz -= 0.1;
       else
          w -= 0.1;
    }
+   else{
+      return;
+   }
    //  Tell GLUT it is necessary to redisplay the scene
+   printf("hi");
    glutPostRedisplay();
 }
 
@@ -188,6 +233,9 @@ void special(int key,int x,int y)
    //  Down arrow key - decrease elevation by 5 degrees
    else if (key == GLUT_KEY_DOWN)
       ph -= 5;
+   else{
+      return;
+   }
    //  Keep angles to +/-360 degrees
    th %= 360;
    ph %= 360;
@@ -229,7 +277,7 @@ int main(int argc,char* argv[])
    //  Request 500 x 500 pixel window
    glutInitWindowSize(500,500);
    //  Create the window
-   glutCreateWindow("Coordinates");
+   glutCreateWindow("Justin Chin HW 1");
    //  Tell GLUT to call "display" when the scene should be drawn
    glutDisplayFunc(display);
   //  Tell GLUT to call "reshape" when the window is resized
