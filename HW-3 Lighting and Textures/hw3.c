b0eecd68b3879feda0ca52aa73fd0eb513cb5948
Justin Chin <justinchin.bcchs@gmail.com>
2018-06-24 13:53:07 -0600

    separated matrix calculations (normal vec,middle vec) into seperate header file

--- HW-3 Lighting and Textures/hw3.c
+++ HW-3 Lighting and Textures/hw3.c
@@ -24,6 +24,7 @@
  *  ESC        Exit
  */
 #include "CSCIx229.h"
+#include "MatrixFunc.h"
 //#include <math.h>
 
 int axes=1;       //  Display axes
@@ -76,43 +77,6 @@ double numStars = 12;
 
 typedef struct {double x,y,z,sx,sy,sz,th,r,g,b;} StarPos;
 StarPos *StarPosArr;
-
-typedef struct{ float x,y,z; }Vector;
-float dotProduct(Vector a, Vector b)
-{
-   return a.x*b.x+a.y*b.y+a.z*b.z;
-}
- 
-Vector crossProduct(Vector a,Vector b)
-{
-   Vector c = {a.y*b.z - a.z*b.y, a.z*b.x - a.x*b.z, a.x*b.y - a.y*b.x};
-   return c;
-}
-
-Vector vecSubtract(Vector a,Vector b)
-{
-   Vector c = {a.x-b.x, a.y-b.y, a.z-b.z};
-   return c;
-}
-
-Vector middleVec(Vector a,Vector b,Vector c)
-{
-   double meanx = (a.x+b.x+c.x)/3.0;
-   double meany = (a.y+b.y+c.y)/3.0;
-   double meanz = (a.z+b.z+c.z)/3.0;
-   Vector d = {meanx,meany,meanz};
-   return d;
-}
-
-Vector normalVec(Vector v1,Vector v2,Vector v3){
-   Vector vec1 = {v1.x,v1.y,v1.z};
-   Vector vec2 = {v2.x,v2.y,v2.z};
-   Vector vec3 = {v3.x,v3.y,v3.z};
-   Vector vecSurf1 = vecSubtract(vec1,vec2);
-   Vector vecSurf2 = vecSubtract(vec1,vec3);
-   Vector normVec = crossProduct(vecSurf1,vecSurf2);
-   return normVec;
-}
 
 typedef struct {double x,y,z;} vertexPos;
 double vertexPosArr[20][9] = {
@@ -727,8 +691,8 @@ void motion(int x,int y)
    //  WARNING:  this only works because by coincidence 1m = 1pixel
    if (move)
    {
-      //  Left/right movement
-      Ox += (X-x)/50.0*Cos(th) - (Y-y)/50.0*Sin(th);
+         //  Left/right movement
+         Ox += (X-x)/50.0*Cos(th) - (Y-y)/50.0*Sin(th);
       //  Near/far or Up/down movement
       if (move<0)
          Oy -= (Y-y)/50.0;
